4ec0904308bc2441a3d97865b038bc4046bee222
Arindam Choudhury <arindam@live.com>
2016-02-25 16:19:36 +0100

    move watch replated files to /usr/local

--- hadoop-master/files/my-service-handler.sh
+++ hadoop-master/files/my-service-handler.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 consul-template -config /usr/local/consul-watch/template.cfg -once
 echo "hulu" >>/usr/local/consul-watch/hulu
-./usr/local/consul-watch/watch-helper.py
+/usr/local/consul-watch/watch-helper.py
