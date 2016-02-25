4b6be599e4cffe3eccf56995316005e61090cd00
Arindam Choudhury <arindam@live.com>
2016-02-25 17:03:05 +0100

    lets try

--- hadoop-master/files/my-service-handler.sh
+++ hadoop-master/files/my-service-handler.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 consul-template -config /usr/local/consul-watch/template.cfg -once
-echo "hulu" >>/usr/local/consul-watch/hulu
-/usr/local/consul-watch/watch-helper.py
+
+cp /usr/local/consul-watch/result /usr/local/hadoop-2.7.2/etc/hadoop/yarn.include
+cp /usr/local/consul-watch/result /usr/local/hadoop-2.7.2/etc/hadoop/dfs.include
+cp /usr/local/consul-watch/result /usr/local/hadoop-2.7.2/etc/hadoop/slaves
+
+yarn rmadmin -refreshNodes
+
+hdfs dfsadmin -refreshNodes
