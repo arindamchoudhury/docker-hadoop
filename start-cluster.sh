0e31445637321831e320cda87b1d37abb2cea48a
Arindam Choudhury <arindam@live.com>
2016-02-25 08:15:49 +0000

    user defined cluster name

--- start-cluster.sh
+++ start-cluster.sh
@@ -2,7 +2,7 @@
 
 # run N slave containers
 N=$1
-DOMAIN=${2:-'pepe.local'}
+DOMAIN=${2:-'hadoop-cluster.local'}
 DNS_SEARCH="node.dc1.$DOMAIN"
 
 
@@ -14,14 +14,14 @@ fi
 
 docker rm -f consul-server &> /dev/null
 echo "start consul-server container..."
-docker run -e CONSUL_DOMAIN_NAME=$DOMAIN -e DNS_SEARCH=$DNS_SEARCH --name=consul-server -h server arindamchoudhury/consul-server  -bootstrap &> /dev/null
+docker run -e CONSUL_DOMAIN_NAME=$DOMAIN --name=consul-server -d -h server --dns-search $DNS_SEARCH --dns 127.0.0.1 arindamchoudhury/consul-server  -bootstrap &> /dev/null
 
 SERVER_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" consul-server)
 
 # delete old master container and start new master container
 docker rm -f master &> /dev/null
 echo "start master container..."
-docker run -e CONSUL_DOMAIN_NAME=$DOMAIN -e CONSUL_SERVER_ADDR=$SERVER_IP -e DNS_SEARCH=$DNS_SEARCH -d -h master --name=master arindamchoudhury/hadoop-master &> /dev/null
+docker run -e CONSUL_DOMAIN_NAME=$DOMAIN -e CONSUL_SERVER_ADDR=$SERVER_IP -d -h master --name=master --dns-search $DNS_SEARCH --dns 127.0.0.1 arindamchoudhury/hadoop-master &> /dev/null
 
 # get the IP address of master container
 # delete old slave containers and start new slave containers
@@ -31,7 +31,7 @@ while [ $i -lt $N ]
 do
 	docker rm -f slave$i &> /dev/null
 	echo "start slave$i container..."
-	docker run -e CONSUL_DOMAIN_NAME=$DOMAIN -e CONSUL_SERVER_ADDR=$SERVER_IP -e DNS_SEARCH=$DNS_SEARCH -d -h slave$i --name=slave$i arindamchoudhury/hadoop-slave  &> /dev/null
+	docker run -e CONSUL_DOMAIN_NAME=$DOMAIN -e CONSUL_SERVER_ADDR=$SERVER_IP -d -h slave$i --name=slave$i --dns-search $DNS_SEARCH --dns 127.0.0.1 arindamchoudhury/hadoop-slave  &> /dev/null
 	((i++))
 done 
 
