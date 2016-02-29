52c1e56253a98d52e8ab74ca8bacc70b73e471aa
Arindam Choudhury <arindam@live.com>
2016-02-29 18:15:41 +0100

    destroy none images

--- destroy_images.sh
+++ destroy_images.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker rmi arindamchoudhury/hadoop-master arindamchoudhury/hadoop-slave arindamchoudhury/hadoop-base
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+
