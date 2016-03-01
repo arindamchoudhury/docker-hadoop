92c8ce2e57b5173ef673de62b425189b907d2c6d
Arindam Choudhury <arindam@live.com>
2016-03-01 14:49:32 +0100

    try bootstrap because I can

--- hadoop-master/files/bootstrap.sh
+++ hadoop-master/files/bootstrap.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 
 service ssh start
-sleep 2
 
-/usr/sbin/ntpd -u ntp:ntp -p /var/run/ntpd.pid -g -n
-sleep 2
+ervice ntp start
 
 nohup /usr/local/consul/bin/consul agent -config-dir /usr/local/consul/config --domain=$CONSUL_DOMAIN_NAME -join $CONSUL_SERVER_ADDR >>/var/log/consul.log 2>&1 &
-sleep 2
 
 sudo -E -u hdfs /usr/local/hadoop-2.7.2/sbin/hadoop-daemon.sh start namenode
-sleep 2
 
 sudo -E -u yarn /usr/local/hadoop-2.7.2/sbin/yarn-daemon.sh start resourcemanager
-sleep 2
 
 sudo -E -u yarn /usr/local/hadoop-2.7.2/sbin/yarn-daemon.sh start historyserver
-sleep 2
 
 if [[ $1 == "-d" ]]; then
-  while true; do sleep 10; done
+  while true; do sleep 1000; done
 fi
 
 if [[ $1 == "-bash" ]]; then
--- hadoop-slave/files/bootstrap.sh
+++ hadoop-slave/files/bootstrap.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
 service ssh start
-sleep 2
 
-/usr/sbin/ntpd -u ntp:ntp -p /var/run/ntpd.pid -g -n
-sleep 2
+ervice ntp start
 
 nohup /usr/local/consul/bin/consul agent -config-dir /usr/local/consul/config --domain=$CONSUL_DOMAIN_NAME -join $CONSUL_SERVER_ADDR >>/var/log/consul.log 2>&1 &
-sleep 2
 
 sudo -E -u hdfs /usr/local/hadoop-2.7.2/sbin/hadoop-daemon.sh start datanode
-sleep 2
-
 sudo -E -u yarn /usr/local/hadoop-2.7.2/sbin/yarn-daemon.sh start nodemanager
-sleep 2
-
 
 if [[ $1 == "-d" ]]; then
-  while true; do sleep 10; done
+  while true; do sleep 1000; done
 fi
 
 if [[ $1 == "-bash" ]]; then
