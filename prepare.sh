ea2442721be0820895112b048b8c5198268475ca
Arindam Choudhury <arindam@live.com>
2016-03-02 13:15:08 +0100

    cleaned

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+mkdir -p consul/tars
 mkdir -p hadoop-base/tars
 mkdir -p hadoop-hive/tars
 
-cd hadoop-base/tars
+cd consul/tars
+
+wget -c -O "consul_0.6.3_linux_amd64.zip" "https://releases.hashicorp.com/consul/0.6.3/consul_0.6.3_linux_amd64.zip"
+
+cd ../../hadoop-base/tars
 
 wget -c -O "jdk-7u80-linux-x64.tar.gz" --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz"
 wget -c -O "hadoop-2.7.2.tar.gz" "http://apache.rediris.es/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz"
